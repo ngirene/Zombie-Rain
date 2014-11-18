0983f233b20a2a83276acd5ba907856342c74999
Anthony Tran <anthont2@uci.edu>
2014-11-17 16:36:44 -0800

    Update Item.java

--- Item.java
+++ Item.java
@@ -2,6 +2,22 @@
 
 public abstract class Item
 {
+	//item ID will decide which type of item it is
+	//0 will be Invincibility
+	//1 will be Wipe Out
+	//2 will be Ground Level Wipe Out
+	//3 will be Extra Heart
+	//4 will be Unicorn Horn
+	//5 will be Sidekick
+	//6 will be Red Bull
+	//7 will be 'MURICA/CHUCK NORRIS
+	//10 will be Take Away Heart
+	//11 will be Weak Shot
+	//12 will be Immobilize
+	//13 will be Decreased Range
+	//20 will be Knives
+	//21 will be Machine Gun
+	//22 will be Bazooka
 	private int itemID;
 	
 	public static final int DEFAULT_ID = 0;
