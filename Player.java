6d5e7251a0412f846326cbf5d240dfe0e9f27141
Anthony Tran <anthont2@uci.edu>
2014-11-30 01:27:44 -0800

    Create Player.java
    Fabio's code

---
+++ Player.java
@@ -0,0 +1,14 @@
+package EntityClasses;
+
+public class Player {
+	
+	private int hearts;
+	private int score;
+
+	public Player() {
+		// TODO Auto-generated constructor stub
+		this.hearts = 3;
+		this.score = 0;
+	}
+
+}
