aee993322a97866f4e617167a78d95687ce63e75
Anthony Tran <anthont2@uci.edu>
2014-12-16 18:13:17 -0800

    Updated Player class

--- Player.java
+++ Player.java
@@ -1,14 +1,51 @@
-package com.zombier;
+package EntityClasses;
+
+import java.util.ArrayList;
+
+import cs113.owl.zombie.rain.Item;
 
 public class Player {
 	
 	private int hearts;
 	private int score;
-
+	private ArrayList<Item> items;
+	
 	public Player() {
 		// TODO Auto-generated constructor stub
 		this.hearts = 3;
 		this.score = 0;
+		ArrayList<Item> items = new ArrayList<Item>();
+	}
+	public int getHearts() {
+		return hearts;
+	}
+
+	public void setHearts(int hearts) {
+		this.hearts = hearts;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public ArrayList<Item> getItems() {
+		return items;
+	}
+
+	public void setItems(ArrayList<Item> items) {
+		this.items = items;
+	}
+	
+	public void addItems(Item item){
+		this.items.add(item);
+	}
+	
+	public void deleteItems(Item item){
+		this.items.remove(item);
 	}
 
 }
